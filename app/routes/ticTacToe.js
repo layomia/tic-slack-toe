0b3ea6fdb638a034d2b962838c4c4b408e19ca00
Oluwalayomi Akinrinade <layomia@gmail.com>
2016-09-28 10:37:51 -0400

    Added team_id authentication

--- app/routes/ticTacToe.js
+++ app/routes/ticTacToe.js
@@ -3,7 +3,9 @@
 var Game = require("../models/game");
 
 var slackToken = "nqa4K3GFPfj2ULmjRCpEKuXS";
-var slackTeamIDs = [""];
+var slackTeamIDs = ["T2G3MTK3K", "T0001"];
+
+
 
 module.exports = function(app, express) {
 	
@@ -14,12 +16,21 @@ module.exports = function(app, express) {
 		// authenticate request here
 		var token = req.body.token || req.param('token') || req.headers['x-access-token'];
 		
-		if (token) {
+		var team_id = req.body.team_id || req.param('team_id') || req.headers['team_id'];
+		
+		
+		if (token && team_id) {
 			if (token != slackToken) {
 				// return appropriate error
 				return res.status(403).send({
 					success: false,
 					message: 'Failed to authenticate token.'
+				});
+			} else if (!(slackTeamIDs.indexOf(team_id) > -1)) {
+				// return appropriate error
+				return res.status(403).send({
+					success: false,
+					message: 'Failed to authenticate team_id.'
 				});
 			} else {
 				next();
@@ -33,8 +44,8 @@ module.exports = function(app, express) {
 		}
 	});
 	
-	//root response for tictactoe
-	//accessed at https://layomia-ttt.herokuapp.com/
+	// root response for tictactoe
+	// accessed at https://layomia-ttt.herokuapp.com/
 	ticTacToeRouter.get("/", function(req, res) {
 		res.json({ message: "Welcome to TicTacToe!" });
 	});
@@ -42,7 +53,6 @@ module.exports = function(app, express) {
 	ticTacToeRouter.route("/play")
 	
 		.post(function(req, res) {
-			console.log(req.body);
 			res.json({ 
 				'response_type': "in_channel",
 				'text': "Wassappppp!"
