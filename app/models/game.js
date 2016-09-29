22e40e0a0c22f23fc9d14478f9969f3b5f2b0c79
Oluwalayomi Akinrinade <layomia@gmail.com>
2016-09-28 21:00:19 -0400

    Preliminary command parsing logic, game/board logic. Pushing to test board display rendering in slack channel

--- app/models/game.js
+++ app/models/game.js
@@ -1,13 +1,61 @@
 //  game.js
 
-// Constructor
-function Game() {
-  // always initialize all instance properties
-  this.current = 0
-}
-// class methods
-Game.prototype.makeMove = function() {
+var x = 'X';
+var o = 'O';
 
+// Constructor
+function Game(p1, p2) {
+	this.rows = 3
+	this.columns = 3
+	this.board = [['1','2','3'], ['4','5','6'], ['7','8','9']];
+	this.players = {0:p1, 1:p2};
+	this.current = 1;
+	this.winner = null;
+}
+
+// class methods
+
+// (re-)initialize board for new game
+Game.prototype.initialize = function(p1, p2) {
+	
+}
+
+// make move
+Game.prototype.makeMove = function(move) {
+	// if 
 };
+
+// display board
+Game.prototype.displayBoard = function() {
+	var board = "";
+	var prompt = "It is the turn of " + this.players[this.current] + "\n";
+	
+	for (var i = 0; i < this.rows; i++){
+        
+        for (var j = 0; j < this.columns; j++){
+            board += " " + "---";
+        }
+        
+        board += "\n";
+        
+        for (var j = 0; j < this.columns; j++){
+            board += "| " + this.board[i][j];
+            board += " ";
+        }
+        
+        board += "|\n"
+        
+    }
+    
+    for (var j = 0; j < this.columns; j++){
+        board += " " + "---";
+    }
+    
+    board += "\n";
+	
+	return board + prompt;
+};
+
+
 // export the class
 module.exports = Game;
--- app/routes/ticTacToe.js
+++ app/routes/ticTacToe.js
@@ -5,7 +5,102 @@ var Game = require("../models/game");
 var slackToken = "nqa4K3GFPfj2ULmjRCpEKuXS";
 var slackTeamIDs = ["T2G3MTK3K", "T0001"];
 
+var games = {};
 
+function validUserName(str) {
+	if (!(str.length > 1 && str[0] == '@')){
+		return false;
+	}
+	
+	var userNameRegex = /^[a-z0-9][a-z0-9._-]*$/;
+	
+	return userNameRegex.test(str.substring(1))
+}
+
+var responses = {
+	'invalidCommand': { 
+		'text': "Invalid command",
+		'attachments': [{'text':"Try `/ttt help`."}]
+	}, 
+	'invalidCreate': { 
+		'text': "A game is currently in play",
+		'attachments': [{'text':"`/ttt quit` to quit game."}]
+	}, 
+	'invalidCreateCoPlayer': { 
+		'text': "Invalid `create` command",
+		'attachments': [{'text':"`/ttt create @username`. Remember, you can't challenge yourself!"}]
+	}, 
+};
+
+// this function assumes that the user_name for both players will not change while a game is being played
+function createGame(user, args) {
+	console.log(games);
+	// if games is not empty i.e, return 
+	if (Object.keys(games).length != 0) {
+		//games['Steve'].makeMove()
+		return responses['invalidCreate'];
+	} else if (args.length != 1) {
+		return responses['invalidCreateCoPlayer'];
+	}
+	
+	var p1 = '@' + user;
+	var p2 = args[0];
+	
+	if (!validUserName(p2) || p1 == p2) {
+		return responses['invalidCreateCoPlayer'];
+	}
+	
+	var game = new Game(p1, p2);
+	
+	games[p1] = game;
+	games[p2] = game;
+	
+	var boardStatus = game.displayBoard();
+	return {
+		'response_type': "in_channel",
+		'text': boardStatus,
+		'attachments': [{'text':"To make move, `/ttt play (board position)`."}]
+	};
+}
+
+function makeMove(user, args) {
+	return {};
+}
+
+function viewBoardStatus(user, args) {
+	return {};
+}
+
+function quitGame(user, args) {
+	return {};
+}
+
+function help(user, args) {
+	return {};
+}
+
+var commands = {
+	'create': createGame,
+	'move': makeMove,
+	'view': viewBoardStatus, 
+	'quit': quitGame,
+	'help': help
+}
+
+function executeCommand(reqBody) {
+	var user = reqBody.user_name;
+	var commandArray = reqBody.text.split(" ");
+	var command = commandArray[0];
+	var args = commandArray.splice(1);
+	
+	var func = commands[command];
+
+	if (func === undefined) {
+		return responses['invalidCommand'];
+	}
+	
+	return func(user, args);
+}
 
 module.exports = function(app, express) {
 	
@@ -53,10 +148,8 @@ module.exports = function(app, express) {
 	ticTacToeRouter.route("/play")
 	
 		.post(function(req, res) {
-			res.json({ 
-				'response_type': "in_channel",
-				'text': "Wassappppp!"
-			});	
+			commandResponse = executeCommand(req.body);	
+			res.json(commandResponse);	
 		})
 
 	return ticTacToeRouter;
